a3efd6584c2da6c3f2a7eb6a680049f668eb51b1
ZaneBloom <u12236722@tuks.co.za>
2014-06-24 21:36:41 +0200

    Deleting weird project structure

---
+++ QuantCommunitySecurity/Tests/SequentialPipelineTest.cpp
@@ -0,0 +1,91 @@
+#include "SequentialPipelineTest.h"
+
+void SequentialPipelineTest::attachAndDetachCapturerTest()
+{
+    Capturer* expected = new FakeCapturer(NULL);
+    Pipeline* pipeline = new SequentialPipeline();
+    pipeline->attachCapturer(expected);
+    Capturer* actual = pipeline->detachCapturer();
+    QCOMPARE(actual, expected);
+    actual = pipeline->detachCapturer();
+    QVERIFY(actual == NULL);
+    pipeline->attachCapturer(expected);
+    actual = pipeline->detachCapturer();
+    QCOMPARE(actual,expected);
+}
+
+void SequentialPipelineTest::attachAndDetachPersisterTest()
+{
+    Persister* expected = new FakePersister();
+    Pipeline* pipeline = new SequentialPipeline();
+    pipeline->attachPersister(expected);
+    Persister* actual = pipeline->detachPersister();
+    QCOMPARE(actual, expected);
+    actual = pipeline->detachPersister();
+    QVERIFY(actual == NULL);
+    pipeline->attachPersister(expected);
+    actual = pipeline->detachPersister();
+    QCOMPARE(actual,expected);
+}
+
+void SequentialPipelineTest::attachAndDetachFilterTest()
+{
+    Pipeline* pipeline = new SequentialPipeline();
+    Filter* expectedFilter = new FakeFilter();
+    pipeline->attachFilter(expectedFilter);
+    QCOMPARE(pipeline->getNumberOfFilters(), 1);
+    Filter* actualFilter = pipeline->detachLastFilter();
+    QCOMPARE(actualFilter,expectedFilter);
+    QVERIFY(pipeline->detachLastFilter() == NULL);
+    QCOMPARE(pipeline->getNumberOfFilters(), 0);
+
+    vector<Filter*> fakeFilters;
+    for (int i = 0; i < 5; i++)
+    {
+        fakeFilters.push_back(new FakeFilter());
+    }
+    pipeline->attachFilters(fakeFilters);
+    pipeline->attachFilter(expectedFilter);
+    int expectedNumFilters = 6;
+    QCOMPARE(pipeline->getNumberOfFilters(), expectedNumFilters);
+    QCOMPARE(pipeline->detachLastFilter(), expectedFilter);
+    QCOMPARE(pipeline->getNumberOfFilters(), --expectedNumFilters);
+    for (int i = 4; i >= 0; i--)
+    {
+        QCOMPARE(pipeline->detachLastFilter(), fakeFilters[i]);
+        QCOMPARE(pipeline->getNumberOfFilters(), --expectedNumFilters);
+    }
+}
+
+void SequentialPipelineTest::processTest()
+{
+    FakeCapturer* fakeCapturer = new FakeCapturer(new ImageData());
+    FakePersister* fakePersister = new FakePersister();
+    vector<Filter*> fakeFilters;
+    for (int i = 0; i < 5; i++)
+    {
+        fakeFilters.push_back(new FakeFilter());
+    }
+
+    Pipeline* pipeline= new SequentialPipeline();
+    pipeline->attachCapturer(fakeCapturer);
+    pipeline->attachPersister(fakePersister);
+    pipeline->attachFilters(fakeFilters);
+
+    pipeline->process(1);
+
+    QVERIFY(fakeCapturer->getCalledFlag());
+    QVERIFY(fakePersister->getCalledFlag());
+    for (int i = 0; i < 5; i++)
+    {
+        FakeFilter* fakeFilter = dynamic_cast<FakeFilter*>(fakeFilters[i]);
+        QVERIFY(fakeFilter->getCalledFlag());
+    }
+}
+
+//QTEST_APPLESS_MAIN(SequentialPipelineTest)
+
+//#include "tst_SequentialPipelineTest.moc"
+
+QTEST_MAIN(SequentialPipelineTest)
+#include "moc_SequentialPipelineTest.cpp"
