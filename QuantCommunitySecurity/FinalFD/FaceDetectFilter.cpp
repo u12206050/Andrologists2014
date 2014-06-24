30f4c20acf0e1819bbfa215541aa718fddd6fb10
Heelin Mistry - u10299344 <hmistry69@gmail.com>
2014-06-24 18:23:12 +0200

    Update FaceDetect cpp

--- QuantCommunitySecurity/FinalFD/FaceDetectFilter.cpp
+++ QuantCommunitySecurity/FinalFD/FaceDetectFilter.cpp
@@ -21,24 +21,29 @@ FaceDetectFilter::~FaceDetectFilter()
 ImageData* FaceDetectFilter::filter(ImageData* image)
 {
     Mat frame = image->image;
-    int im_width = frame.cols;
-    int im_height = frame.rows;
 
     // Detect faces
     vector<Rect> faces;
     face_cascade.detectMultiScale( frame, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
+
     for(int i = 0; i < faces.size(); i++) {
         Rect face_i = faces[i];
         Mat face = frame(face_i);
 
+/*
+        int im_width = frame.width;
+        int im_height = frame.height;
+
         Mat face_resized;
         resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
+*/
 
         //identify faces in frame green rectangle
         //rectangle(frame, face_i, CV_RGB(0, 255,0), 1);
-        image->faces.push_back(face_resized);
+        image->faces.push_back(face);
     }
+
     return image;
 }
 
