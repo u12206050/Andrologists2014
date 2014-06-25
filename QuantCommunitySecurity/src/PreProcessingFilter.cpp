7b16221be9d7c14ab9395d3961caa252e36511f5
ZaneBloom <u12236722@tuks.co.za>
2014-06-25 16:18:34 +0200

    Started with PreProcessing

--- QuantCommunitySecurity/src/PreProcessingFilter.cpp
+++ QuantCommunitySecurity/src/PreProcessingFilter.cpp
@@ -1,38 +1,28 @@
 #include "PreProcessingFilter.h"
 
+PreProcessingFilter::PreProcessingFilter(int targetWidth, int targetHeight)
+{
+    this->targetWidth = targetWidth;
+    this->targetHeight = targetHeight;
+}
+
 PreProcessingFilter::~PreProcessingFilter()
 {
-    /*IplImage *imageGrey;
-    if (imageSrc->nChannels == 3) {
-        imageGrey = cvCreateImage( cvGetSize(imageSrc), IPL_DEPTH_8U, 1 );
-        // Convert from RGB (actually it is BGR) to Greyscale.
-        cvCvtColor( imageSrc, imageGrey, CV_BGR2GRAY );
-    }
-    else {
-        // Just use the input image, since it is already Greyscale.
-        imageGrey = imageSrc;
-    }
 
-    // Resize the image to be a consistent size, even if the aspect ratio changes.
-    IplImage *imageProcessed;
-    imageProcessed = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 1);
-    // Make the image a fixed size.
-    // CV_INTER_CUBIC or CV_INTER_LINEAR is good for enlarging, and
-    // CV_INTER_AREA is good for shrinking / decimation, but bad at enlarging.
-    cvResize(imageGrey, imageProcessed, CV_INTER_LINEAR);
-
-    // Give the image a standard brightness and contrast.
-    cvEqualizeHist(imageProcessed, imageProcessed);
-
-
-
-    if (imageGrey)
-        cvReleaseImage(&imageGrey);
-    if (imageProcessed)
-        cvReleaseImage(&imageProcessed);*/
 }
 
 ImageData* PreProcessingFilter::filter(ImageData* image)
 {
-    return NULL;
+    for (unsigned int i = 0; i < image->faces.size(); i++)
+    {
+        preProcessImage(image->faces[i]);
+    }
+    return image;
+}
+
+void PreProcessingFilter::preProcessImage(Mat& image)
+{
+    resize(image, image, Size(targetWidth, targetHeight), 1.0, 1.0, INTER_CUBIC);
+    cvtColor(image, image, CV_BGR2GRAY);
+    equalizeHist(image, image);
 }
--- QuantCommunitySecurity/src/PreProcessingFilter.h
+++ QuantCommunitySecurity/src/PreProcessingFilter.h
@@ -1,13 +1,22 @@
 #if !defined(_PREPROCESSINGFILTER_H)
 #define _PREPROCESSINGFILTER_H
 
+#include "opencv2/imgproc/imgproc.hpp"
 #include "Filter.h"
+
+using namespace cv;
 
 class PreProcessingFilter : public Filter
 {
-public:
-	~PreProcessingFilter();
-	ImageData* filter(ImageData* image);
+    public:
+        PreProcessingFilter(int targetWidth, int targetHeight);
+        ~PreProcessingFilter();
+        ImageData* filter(ImageData* image);
+
+    private:
+        void preProcessImage(Mat& image);
+        int targetWidth;
+        int targetHeight;
 };
 
 #endif  //_PREPROCESSINGFILTER_H
