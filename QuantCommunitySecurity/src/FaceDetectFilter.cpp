d2b1baff220fb3189a1aacc92b1ddd74d32dcc5a
Heelin Mistry - u10299344 <hmistry69@gmail.com>
2014-07-01 12:24:21 +0200

    Updated FaceDetectFilter

--- QuantCommunitySecurity/src/FaceDetectFilter.cpp
+++ QuantCommunitySecurity/src/FaceDetectFilter.cpp
@@ -8,7 +8,7 @@ FaceDetectFilter::FaceDetectFilter(String xmlLoc)
     face_cascade.load(xmlLoc);
     if(face_cascade.empty())
     {
-        throw exception();
+        throw Exception();
     }
 }
 
@@ -21,17 +21,17 @@ ImageData* FaceDetectFilter::filter(ImageData* image)
 {
     Mat frame = image->image;
 
+    image->faces.clear();
+
     // Detect faces
     vector<Rect> faces;
     //Mat gray_image;
     //cvtColor( frame, gray_image, CV_BGR2GRAY );
     face_cascade.detectMultiScale( frame, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
-
     for(unsigned int i = 0; i < faces.size(); i++) {
         Rect face_i = faces[i];
         Mat face = frame(face_i);
-
 /*
         int im_width = frame.width;
         int im_height = frame.height;
@@ -39,9 +39,8 @@ ImageData* FaceDetectFilter::filter(ImageData* image)
         Mat face_resized;
         resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
 */
-
         //identify faces in frame green rectangle
-        //rectangle(frame, face_i, CV_RGB(0, 255,0), 1);
+        rectangle(frame, face_i, CV_RGB(0, 255,0), 1);
         image->addFace(face);
     }
 
